b8ffd933ad12252bd9754a9572b70860baa9d083
junsungkim96 <junsung.k96@gmail.com>
2025-04-23 15:44:28 +0900

    Fix error for Vercel analytics

--- src/App.js
+++ src/App.js
@@ -44,54 +44,56 @@ const MainLayout = ({ children }) => (
 );
 
 const App = () => (
-  <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {/* Homepage */}
-        <Route path="/" element={<MainLayout>< Homepage /></MainLayout>} />
-        
-        {/* Product */}
-        <Route path="/product_features" element={<MainLayout>< Product /></MainLayout>}/>
-        <Route path="/product_pricing" element={<MainLayout>< Pricing /></MainLayout>}/>
-        
-        {/* Research */}
-        <Route path="/research" element={<MainLayout>< Research /></MainLayout>}/>
-        <Route path="/illuminant" element={<MainLayout>< Illuminant /></MainLayout>}/>
-        <Route path="/optics" element={<MainLayout>< Optics /></MainLayout>}/>
-        <Route path="/sensor" element={<MainLayout>< Sensor /></MainLayout>}/>
-        <Route path="/isp" element={<MainLayout>< ISP /></MainLayout>}/>
-        <Route path="/qpu" element={<MainLayout>< QPU /></MainLayout>}/>
-        
-        {/* Company */}
-        <Route path="/company" element={<MainLayout>< Company /></MainLayout>} />
-        <Route path="/blog" element={<MainLayout>< Blog /></MainLayout>}/>
-        <Route path="/careers" element={<MainLayout>< Careers /></MainLayout>} />
-        <Route path="/stories" element={<MainLayout>< Stories /></MainLayout>} />
-        <Route path="/ir" element={<MainLayout>< IR /></MainLayout>} />
-        <Route path="/news" element={<MainLayout>< News /></MainLayout>} />
+  <>
+    {/* Vercel Analytics for Website */}
+    <Analytics/>
 
-        {/* Company Blog */}
-        <Route path="/blog/founding_story" element={<MainLayout>< FoundingStory /></MainLayout>}/>
-        <Route path="/blog/cv" element={<MainLayout>< CV /></MainLayout>}/>
-        
-        {/* Signup & Apply */}
-        <Route path="/login" element={< Login />} />
-        <Route path="/signup" element={< Signup />} />
-        <Route path="/apply" element={< ApplicationFrom />} />
-        <Route path="/submit" element={< Submit /> } />
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Homepage */}
+          <Route path="/" element={<MainLayout>< Homepage /></MainLayout>} />
+          
+          {/* Product */}
+          <Route path="/product_features" element={<MainLayout>< Product /></MainLayout>}/>
+          <Route path="/product_pricing" element={<MainLayout>< Pricing /></MainLayout>}/>
+          
+          {/* Research */}
+          <Route path="/research" element={<MainLayout>< Research /></MainLayout>}/>
+          <Route path="/illuminant" element={<MainLayout>< Illuminant /></MainLayout>}/>
+          <Route path="/optics" element={<MainLayout>< Optics /></MainLayout>}/>
+          <Route path="/sensor" element={<MainLayout>< Sensor /></MainLayout>}/>
+          <Route path="/isp" element={<MainLayout>< ISP /></MainLayout>}/>
+          <Route path="/qpu" element={<MainLayout>< QPU /></MainLayout>}/>
+          
+          {/* Company */}
+          <Route path="/company" element={<MainLayout>< Company /></MainLayout>} />
+          <Route path="/blog" element={<MainLayout>< Blog /></MainLayout>}/>
+          <Route path="/careers" element={<MainLayout>< Careers /></MainLayout>} />
+          <Route path="/stories" element={<MainLayout>< Stories /></MainLayout>} />
+          <Route path="/ir" element={<MainLayout>< IR /></MainLayout>} />
+          <Route path="/news" element={<MainLayout>< News /></MainLayout>} />
 
-        {/* Contact, Terms of Use & Privacy */}
-        <Route path="/contact" element={<MainLayout>< Contact /></MainLayout>}/>
-        <Route path="/terms" element={<MainLayout>< Terms /></MainLayout>}/>
-        <Route path="/privacy" element={<MainLayout>< Privacy /></MainLayout>}/>
-        <Route path="/contact_sales" element = {<MainLayout>< ContactSales /></MainLayout>}/>
+          {/* Company Blog */}
+          <Route path="/blog/founding_story" element={<MainLayout>< FoundingStory /></MainLayout>}/>
+          <Route path="/blog/cv" element={<MainLayout>< CV /></MainLayout>}/>
+          
+          {/* Signup & Apply */}
+          <Route path="/login" element={< Login />} />
+          <Route path="/signup" element={< Signup />} />
+          <Route path="/apply" element={< ApplicationFrom />} />
+          <Route path="/submit" element={< Submit /> } />
 
-        {/* Vercel Analytics for Website */}
-        <Analytics/>
+          {/* Contact, Terms of Use & Privacy */}
+          <Route path="/contact" element={<MainLayout>< Contact /></MainLayout>}/>
+          <Route path="/terms" element={<MainLayout>< Terms /></MainLayout>}/>
+          <Route path="/privacy" element={<MainLayout>< Privacy /></MainLayout>}/>
+          <Route path="/contact_sales" element = {<MainLayout>< ContactSales /></MainLayout>}/>
 
-      </Routes>
-    </Suspense>
-  </Router>
+        </Routes>
+      </Suspense>
+    </Router>
+  </>
 );
 
 export default App;
