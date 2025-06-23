7994b74475efe80f53c75e66f13aa92b50e52226
junsungkim96 <junsung.k96@gmail.com>
2025-06-23 11:55:59 +0900

    Delete program and replaced with Vercel blob storage url download link

--- api/license-check.js
+++ api/license-check.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
       // Respond with a success message and download link
       return res.status(200).json({
         valid: true,
-        downloadUrl: '/program/QuasarVisionInstaller.zip',
+        downloadUrl: 'https://8puowjmkndisb60h.public.blob.vercel-storage.com/QuasarVisionInstaller-uTGg5uSOIimYhU0vfWpccpBQDuw20p.zip',
       });
     } else {
       // If credentials are invalid, return unauthorized
