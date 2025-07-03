1de6bdc89e84dc6024bfa1f28cd40fbebe07b070
junsungkim96 <junsung.k96@gmail.com>
2025-07-04 00:26:16 +0900

    Changed download file url

--- api/license-check.js
+++ api/license-check.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
       // Respond with a success message and download link
       return res.status(200).json({
         valid: true,
-        downloadUrl: 'https://8puowjmkndisb60h.public.blob.vercel-storage.com/QuasarVision_setup-BgOnPQg7CcrSsc6LhAzyryiU8KUSiJ.zip',
+        downloadUrl: 'https://8puowjmkndisb60h.public.blob.vercel-storage.com/QuasarVision_setup-Wte64rRy3A2JZPvNHLOAinGmkrtZpu.zip',
       });
     } else {
       // If credentials are invalid, return unauthorized
