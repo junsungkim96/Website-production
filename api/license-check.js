6138493d2f8beb3c01dbb6578bdf88d08868e1a2
junsungkim96 <junsung.k96@gmail.com>
2025-07-06 14:30:04 +0900

    Update to latest download file url with threading

--- api/license-check.js
+++ api/license-check.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
       // Respond with a success message and download link
       return res.status(200).json({
         valid: true,
-        downloadUrl: 'https://8puowjmkndisb60h.public.blob.vercel-storage.com/QuasarVisionInstaller-Njvr0Ieij7KQvZVJ8vAv8zL7KdvpSC.zip',
+        downloadUrl: 'https://8puowjmkndisb60h.public.blob.vercel-storage.com/QuasarVisionInstaller-HnZL3r1YWzYIgvHBtW5SyD9P81tnHL.zip',
       });
     } else {
       // If credentials are invalid, return unauthorized
